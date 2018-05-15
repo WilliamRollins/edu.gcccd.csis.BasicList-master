39184703a9895e07466acbe51d52b05d61861fa1
William.K.Rollins <36085204+WilliamRollins@users.noreply.github.com>
2018-05-15 07:01:04 -0700

    added commented test cases

--- src/test/java/edu/gcccd/csis/MyProject2Test.java
+++ src/test/java/edu/gcccd/csis/MyProject2Test.java
@@ -7,39 +7,46 @@ import static org.junit.Assert.assertEquals;
 
 public class MyProject2Test {
 
+    final Project2 p = new MyProject2();
+    final NodeList<Integer> nEmpty = new NodeList<>();
+    final NodeList<Integer> n0 = new NodeList<>();
+    final NodeList<Integer> n1 = new NodeList<>();
+    final NodeList<Integer> n2 = new NodeList<>();
+    final NodeList<Integer> n3 = new NodeList<>();
+    final NodeList<Integer> n9 = new NodeList<>();
+    final NodeList<Integer> n99 = new NodeList<>();
+n0.append(0);n0.append(0);n0.append(0);
+n1.append(1);
+n2.append(1); n2.append(2);
+n3.append(1);n3.append(2);n3.append(3);
+n9.append(9);
+n99.append(9);n99.append(9);
     @Before
     public void setUp() throws Exception {
-        final Project2 p = new MyProject2();
-        final NodeList<Integer> n1 = new NodeList<>();
-        final NodeList<Integer> n2 = new NodeList<>();
-        n1.append(9);
-
-        int k = 2;
-        for (int i = 0; i < k; i++) {
-            n2.append(9);
-        }
     }
 
     @Test
     public void reverse() {
+        n0.append(0);n0.append(0);n0.append(0);
+        n1.append(1);
+        n2.append(1); n2.append(2);
+        n3.append(1);n3.append(2);n3.append(3);
+
         //reverse 1 = 1;
-        //reverse 12=21
+        MyProject2.reverse(n1.iterator(), n1);
+        //reverse 1,2=2,1
+        MyProject2.reverse(n2.iterator(), n2);
         //reverse 123=321
+        MyProject2.reverse(n3.iterator(), n3);
         //reverse 000=000
+        MyProject2.reverse(n0.iterator(), n0);
     }
 
     @Test
     public void addition() {
 //test both nodeList are null
         final Project2 p = new MyProject2();
-        NodeList<Integer> nEmpty =new NodeList<>();
-        NodeList<Integer> n0=new NodeList<>();
-        NodeList<Integer> n9=new NodeList<>();
-        NodeList<Integer> n99=new NodeList<>();
-        n0.append(0);
-        n9.append(0);n9.append(9);
-        n99. append(9); n99. append(9);
-        Project2.print(n0);
+
 
         // test one node list is null
         //assertEquals("node 1 and 2 can not be empty", p.addition(nEmpty,nEmpty));
@@ -55,11 +62,11 @@ public class MyProject2Test {
 
     @Test
     public void addition1() {
-    // test if list is null;
-    //test if list only has one nodeList
-    //test if 0+9=9
-    //test if 9+9+9=27
-    //test if 0+9+99=108;
+        // test if list is null;
+        //test if list only has one nodeList
+        //test if 0+9=9
+        //test if 9+9+9=27
+        //test if 0+9+99=108;
     }
 
     @Test
