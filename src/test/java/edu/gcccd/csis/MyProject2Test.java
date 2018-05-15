16baabf613b2a081d17a403cae57365d6852fb42
William.K.Rollins <36085204+WilliamRollins@users.noreply.github.com>
2018-05-14 21:30:46 -0700

    all MyProject2 methods work. starting test

--- src/test/java/edu/gcccd/csis/MyProject2Test.java
+++ src/test/java/edu/gcccd/csis/MyProject2Test.java
@@ -12,7 +12,6 @@ public class MyProject2Test {
         final Project2 p = new MyProject2();
         final NodeList<Integer> n1 = new NodeList<>();
         final NodeList<Integer> n2 = new NodeList<>();
-
             n1.append(9);
 
         int k=2;
@@ -24,10 +23,24 @@ public class MyProject2Test {
     @Test
     public void reverse() {
         final Project2 p = new MyProject2();
+        final NodeList<Integer> n0 = new NodeList<>();
         final NodeList<Integer> n1 = new NodeList<>();
         final NodeList<Integer> n2 = new NodeList<>();
+        final NodeList<Integer> n3 = new NodeList<>();
+        final NodeList<Integer> n4 = new NodeList<>();
+        final NodeList<Integer> n5 = new NodeList<>();
+        final NodeList<Integer> n6 = new NodeList<>();
+        final NodeList<Integer> n7 = new NodeList<>();
+        final NodeList<Integer> n8 = new NodeList<>();
+        final NodeList<Integer> n9 = new NodeList<>();
+        n1.append(9);
+        int k =9;
+        for (int i=0; i>k; i++) {
+            n9.append(9);
+        }
         System.out.println(p.addition(n1,n2));
-        assertEquals(p.addition(n1,n2),"node 1 or 2 can not be empty" );
+
+        assertEquals(p.addition(n1,n9),999999999 );
     }
 
     @Test
