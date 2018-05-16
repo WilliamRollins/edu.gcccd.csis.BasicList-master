e7cb81fae5062b71cd6eca30375d9aaa201b9ee0
William.K.Rollins <36085204+WilliamRollins@users.noreply.github.com>
2018-05-15 17:03:35 -0700

    changed addition method. broke for loops out side of echother. addition works now.

--- src/main/java/edu/gcccd/csis/MyProject2.java
+++ src/main/java/edu/gcccd/csis/MyProject2.java
@@ -5,11 +5,11 @@ import java.util.Iterator;
 
 //reverse a nodeList
 public class MyProject2 implements Project2 {
-    public static NodeList<Integer> reverse(Iterator<Integer> iterator, NodeList<Integer> nodeList) {
+    public static NodeList<Integer> reverse(Iterator<Integer> iterator,NodeList<Integer> nodeList) {
         if (iterator.hasNext()) {
             int k = iterator.next();
             nodeList.remove(k);
-            reverse(iterator, nodeList);
+            reverse(iterator,nodeList);
             nodeList.append(k);
         }
         return nodeList;
@@ -17,56 +17,57 @@ public class MyProject2 implements Project2 {
 
     //add 2 nodeList
     @Override
-    public NodeList<Integer> addition(NodeList<Integer> nodeList1, NodeList<Integer> nodeList2) {
-
-        if (nodeList1.getLength()==0 && nodeList2.getLength()==0){
-           throw new IllegalArgumentException("node 1 and 2 can not be empty");
-       }
-        NodeList<Integer> nodeListTotal = new NodeList<>();
-        reverse(nodeList1.iterator(), nodeList1);
-        reverse(nodeList2.iterator(), nodeList2);
-
-        int c = 0;
-        for (int a : nodeList1) {
-            nodeList1.remove(a);
-            for (int b : nodeList2) {
-                nodeList2.remove(b);
-                int nodeTotal = a + b + c;
-                if (nodeTotal > 9 && nodeList1.getLength() > 0 || (nodeTotal > 9 && nodeList2.getLength() > 0)) {
-                    nodeTotal -= 10;
-                    c = 1;
-                }
-                nodeListTotal.append(nodeTotal);
-
-                if (nodeList1.getLength() == 0 && nodeList2.getLength() > 0) {
-                    for (int B : nodeList2) {
-                        nodeList2.remove(B);
-                        if (B + c > 9 && nodeList2.getLength() > 0) {
-                            B -= 10;
-                            c = 1;
-                        }else{c=0;}
-                        B+=c;
-                        nodeListTotal.append(B);
-                    } }
-                if (nodeList2.getLength() == 0 && nodeList1.getLength() > 0) {
-                    for (int A : nodeList1) {
-                        nodeList1.remove(A);
-                        if (A + c > 9 && nodeList1.getLength() > 0) {
-                            A -= 10;
-                            c = 1;
-                        }else{c=0;}
-                        A+=c;
-                        nodeListTotal.append(A);
-                    } }
-                break;
-            } }
-        reverse(nodeListTotal.iterator(), nodeListTotal);
-        for(int node: nodeListTotal){
-            if(node==0){
-                nodeListTotal.remove(0);
-            }
+    public NodeList<Integer> addition(NodeList<Integer> nodeList1,NodeList<Integer> nodeList2) {
+//check to see if null
+        if (nodeList1.getLength() == 0 && nodeList2.getLength() == 0) {
+            throw new IllegalArgumentException("node 1 and 2 can not be empty");
         }
-        return nodeListTotal;
+//set for loop count.
+        int k;
+        if (nodeList1.getLength() > nodeList2.getLength()) {
+            k = nodeList1.getLength();
+        } else {
+            k = nodeList2.getLength();
+        }
+//creats NodeList to hold totals.
+        NodeList<Integer> nodeList3 = new NodeList<>();
+//reverses nodeList to add from right to left.
+        reverse(nodeList1.iterator(),nodeList1);
+        reverse(nodeList2.iterator(),nodeList2);
+//c is used to hold a carryover digit to the next node.
+        int c = 0;
+//iterates through the length of the longest NodeList
+for (int i = 0; i < k; i++) {
+            int n1 = 0;
+            int n2 = 0;
+            int n3 = 0;
+//gets the int in nodelist1 then removes it.
+            for (int node1 : nodeList1) {
+                nodeList1.remove(node1);
+                n1 = node1;
+                break;
+            }
+//gets the int in nodelist2 then removes it.
+            for (int node2 : nodeList2) {
+                nodeList2.remove(node2);
+                n2 = node2;
+                break;
+            }
+//sets int n3 to be added to the NodeList nodeList3. Node* c is = 0 the 1st iteration.
+            n3 = n1 + n2 + c;
+//if n3 is greater than three and there is a node Left to added. preform "carry the one'
+            if (n3 > 9 && nodeList1.getLength() > 0 || n3 > 9 && nodeList2.getLength() > 0) {
+                n3 -= 10;
+                c = 1;
+            } else {
+                c = 0;
+            }
+//append n3 to the nodeList.
+            nodeList3.append(n3);
+        }
+//reverse the nodeList3 so it may be read left to right.
+        reverse(nodeList3.iterator(),nodeList3);
+        return nodeList3;
     }
 
     @Override
@@ -74,21 +75,21 @@ public class MyProject2 implements Project2 {
         NodeList<Integer> nodeListTotal = new NodeList<>();
         nodeListTotal.append(0);
         while (iterator.hasNext()) {
-            nodeListTotal = addition(iterator.next(), nodeListTotal);
+            nodeListTotal = addition(iterator.next(),nodeListTotal);
         }
         return nodeListTotal;
     }
 
     @Override
-    public void save(NodeList<Integer> nodeList, String fileName)  {
-        try{
+    public void save(NodeList<Integer> nodeList,String fileName) {
+        try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
-                for (int x : nodeList) {
-                    writer.write(Integer.toString(x), 0,1);
-                    nodeList.remove(x);
+            for (int x : nodeList) {
+                writer.write(x);
+                nodeList.remove(x);
             }
             writer.close();
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -103,19 +104,21 @@ public class MyProject2 implements Project2 {
                 nodeList.append(nextVal);
                 nextVal = in.read();
             }
+            in.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-    return nodeList;
+        return nodeList;
     }
 
-    public static String nodeString(NodeList<Integer> nodeList){
-        String s="";
-        for(int node: nodeList){
-            s = s+String.valueOf(node);
+    public static String nodeString(NodeList<Integer> nodeList) {
+        String s = "";
+        for (int node : nodeList) {
+            s = s + String.valueOf(node);
         }
         return s;
     }
+
     public static void main(final String[] args) {
         final int L = 3;
 
@@ -124,14 +127,14 @@ public class MyProject2 implements Project2 {
 
         final Project2 p = new MyProject2();
 
-        Project2.print(p.addition(n1, n2)); //  n1+n2, e.g. 4139
+        Project2.print(p.addition(n1,n2)); //  n1+n2, e.g. 4139
 
         final NodeList<NodeList<Integer>> listOfLists = new NodeList<>();
         for (int i = 0; i < L; i++) {
             listOfLists.append(Project2.generateNumber(L));
         }
-        p.save(p.addition(listOfLists.iterator()), "result.bin");
+        p.save(p.addition(listOfLists.iterator()),"results.bin");
         System.out.println("Loading: ");
-        Project2.print(p.load("result.bin"));
+        Project2.print(p.load("results.bin"));
     }
 }
--- src/test/java/edu/gcccd/csis/MyProject2Test.java
+++ src/test/java/edu/gcccd/csis/MyProject2Test.java
@@ -37,8 +37,10 @@ public class MyProject2Test {
         L1.append(n1);
         L2.append(n2);
         L2.append(n2);
-        n9.append(0); n9.append(9);
-        n99.append(9); n99.append(9);
+        n9.append(0);
+        n9.append(9);
+        n99.append(9);
+        n99.append(9);
 
     }
 
@@ -46,11 +48,11 @@ public class MyProject2Test {
     public void reverse() {
         //assertEquals("21", MyProject2.nodeString(MyProject2.reverse(nEmpty.iterator(), nEmpty)));
         //reverse 1 = 1;
-        assertEquals("1", MyProject2.nodeString(MyProject2.reverse(n1.iterator(), n1)));
+        assertEquals("1",MyProject2.nodeString(MyProject2.reverse(n1.iterator(),n1)));
         //reverse 1,2=2,1
-       assertEquals("21", MyProject2.nodeString(MyProject2.reverse(n2.iterator(), n2)));
+        assertEquals("21",MyProject2.nodeString(MyProject2.reverse(n2.iterator(),n2)));
         //reverse 123=321
-        assertEquals("321", MyProject2.nodeString(MyProject2.reverse(n3.iterator(), n3)));
+        assertEquals("321",MyProject2.nodeString(MyProject2.reverse(n3.iterator(),n3)));
 
     }
 
@@ -59,23 +61,23 @@ public class MyProject2Test {
         final Project2 p = new MyProject2();
 
         //test both nodeList are null
-       // assertEquals("node 1 and 2 can not be empty", p.addition(nEmpty, nEmpty));
+       //assertEquals("node 1 and 2 can not be empty", p.addition(nEmpty, nEmpty));
         // test one node list is null
-       // assertEquals("9", MyProject2.nodeString(p.addition(nEmpty,n9)));
+        assertEquals("9", MyProject2.nodeString(p.addition(nEmpty,n9)));
         //test no leading 0's
-        assertEquals("9", MyProject2.nodeString(p.addition(n0,n9)));
+        assertEquals("9",MyProject2.nodeString(p.addition(n0,n9)));
         //test 9+9=18
-        assertEquals("18", MyProject2.nodeString(p.addition(n9,n9)));
+        assertEquals("18",MyProject2.nodeString(p.addition(n9,n9)));
         //test 9+99=108
-        assertEquals("108", MyProject2.nodeString(p.addition(n9,n99)));
+        assertEquals("108",MyProject2.nodeString(p.addition(n9,n99)));
         //0+0=0
-        assertEquals("0", MyProject2.nodeString(p.addition(n0,n0)));
+        assertEquals("0",MyProject2.nodeString(p.addition(n0,n0)));
     }
 
     @Test
     public void addition1() {
         final Project2 p = new MyProject2();
-      // test if list is null;
+        // test if list is null;
         //test if list only has one nodeList
         //test if 9+9=18
         //test if 9+9+9=27
@@ -96,8 +98,8 @@ public class MyProject2Test {
         //load file not there
         //assertEquals("The system cannot find the file specified",String.valueOf(p.load("notThere.bin")));
         //load file open;
-        p.save(p.addition(L3.iterator()), "result.txt");
-                Project2.print(p.load("result.txt"));
+        p.save(p.addition(L3.iterator()),"result.txt");
+        Project2.print(p.load("result.txt"));
 
         //load large NodeList;
     }
